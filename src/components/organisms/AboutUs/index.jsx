40c6df8ddd8e115921a87385ed0e4d03aebbc713
DanushanKanagasingam <danushan@swivelgroup.com.au>
2024-04-25 17:02:54 +0530

    Added para break

--- src/components/organisms/AboutUs/index.jsx
+++ src/components/organisms/AboutUs/index.jsx
@@ -13,11 +13,11 @@ export default function AboutUs() {
           ( Spring Boot), I am very strong at problem solving and crictical
           thinking to solve the problems. Then, I stepped into the Front-End
           world with self-learning, I gained the basic kowledge and then crafted
-          a website from scratched to live product. As a lifelong learner, I am
-          constantly seeking out opportunities to expand my knowledge and stay
-          abreast of the latest technologies and trends in the field. I believe
-          that continuous learning is essential for personal and professional
-          growth.
+          a website from scratched to live product. <br /> As a lifelong
+          learner, I am constantly seeking out opportunities to expand my
+          knowledge and stay abreast of the latest technologies and trends in
+          the field. I believe that continuous learning is essential for
+          personal and professional growth.
         </p>
       </div>
     </div>
